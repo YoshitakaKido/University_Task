d8b097175b41fd6ed74c1e1b886f768b50b2743f
Yoshitaka Kido <yoshitaka.kido@gmail.com>
2016-10-23 16:15:13 +0900

    Update B2.c

--- Computer_Engineering_Laboratory/report2_02/B2.c
+++ Computer_Engineering_Laboratory/report2_02/B2.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int array[] = {1, 2, 4, 8, 16. 32, 64, 128}
+int array[] = {1, 2, 4, 8, 16, 32, 64, 128};
 int main(){
     int sum = 0;
     int *a = array;
@@ -9,7 +9,7 @@ int main(){
         sum = sum + x;
         a++;
         i--;
-    } while(i != 0)
+    } while(i != 0);
     printf("%d", sum);
     return 0;
 }
