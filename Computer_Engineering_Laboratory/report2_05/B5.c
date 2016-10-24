95eba8cea08cf730430bc81c8eee1690c6ab0ef3
Yoshitaka Kido <yoshitaka.kido@gmail.com>
2016-10-24 15:46:28 +0900

    Add files via upload

---
+++ Computer_Engineering_Laboratory/report2_05/B5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int array[15] = {0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+int main(void){
+	int * const from = array + 5;		//配列の5番目の要素のアドレスを格納		
+	int * const to = array;			//配列の先頭要素のアドレスを格納
+	int * p;				//アドレスをコピーするためのポインタを宣言
+	for(p = array; p < array + 15; p++)	
+		printf("%p : %d\n", p, *p);
+	for(p = array; p < array + 10; p++)
+		*p = p[from - to];
+	for(p = array; p < array + 15; p++)
+		printf("%p : %d\n", p, *p);
+	return 0;
+}
