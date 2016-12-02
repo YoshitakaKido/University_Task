cd68f041a79db9bf42fd5d06b0e3bce01efe76a3
Yoshitaka Kido <yoshitaka.kido@gmail.com>
2016-12-02 17:40:02 +0900

    Add files via upload

---
+++ Computer_Engineering_Laboratory/report4/systemverilog-example/work4_3/ex4_3_testbench/_primary.vhd
@@ -0,0 +1,4 @@
+library verilog;
+use verilog.vl_types.all;
+entity ex4_3_testbench is
+end ex4_3_testbench;
---
+++ Computer_Engineering_Laboratory/report4/systemverilog-example/work5_2/ex4_5_testbench/_primary.vhd
@@ -0,0 +1,4 @@
+library verilog;
+use verilog.vl_types.all;
+entity ex4_5_testbench is
+end ex4_5_testbench;
